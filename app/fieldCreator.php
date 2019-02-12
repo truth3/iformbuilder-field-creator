d552957c0e953b795c5a20b0e5b28bcad7c150e1
Tony Ruth <truth@zerionsoftware.com>
2019-02-12 10:14:37 -0500

    Extracted more variables into the keys.php file

--- app/fieldCreator.php
+++ app/fieldCreator.php
@@ -1,7 +1,5 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    die('GET requests are not allowed');
-}
+
 header('Access-Control-Allow-Methods: POST, PUT');
 header("Access-Control-Allow-Headers: X-Requested-With");
 
@@ -9,9 +7,8 @@ require_once "../auth/iFormTokenResolver.php";
 require_once "../auth/keys.php";
 use iForm\Auth\iFormTokenResolver;
 
-global $client,$secret;
-$server = '####';
-$tokenUrl = 'https://' . $server . '.iformbuilder.com/exzact/api/oauth/token';
+// global $client,$secret,$server;
+$tokenUrl = 'https://' . $server . '/exzact/api/oauth/token';
 
 //::::::::::::::  FETCH ACCESS TOKEN   ::::::::::::::
 
@@ -21,9 +18,9 @@ $token = $tokenFetcher->getToken();
 
 
 //:::::::::::::: Get total number of profiles in the DB beyond default reponse by parsing out the response header Total-Count ::::::::::::::
-$profileListUrl = "https://" . $server . ".iformbuilder.com/exzact/api/v60/profiles?limit=1&access_token=" . $token;
-$profileRequestHeaders = (get_headers($profileListUrl)[4]);
-$finalProfileCount = = preg_split("/[\s,]+/", $profileRequestHeaders)[1];
+$profileListUrl = "https://" . $server . "/exzact/api/v60/profiles?limit=1&access_token=" . $token;
+$profileRequestHeaders = (get_headers($profileListUrl,1));
+$finalProfileCount = $profileRequestHeaders["Total-Count"];
 echo("Number of Profiles:" . $finalProfileCount . "\r\n");
 
 //:::::::::::::: Use the total count of profiles to begin looping through each one ::::::::::::::
@@ -36,7 +33,7 @@ for ($i=0; $i<$finalProfileCount; $i++){
 
 
   $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, "https://" . $server . ".iformbuilder.com/exzact/api/v60/profiles?limit=1&offset=$i");
+  curl_setopt($ch, CURLOPT_URL, "https://" . $server . "/exzact/api/v60/profiles?limit=1&offset=$i");
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
@@ -59,9 +56,9 @@ for ($i=0; $i<$finalProfileCount; $i++){
   }
 
     //:::::::::::::: Get total number of pages in the profile beyond default reponse by parsing out the response header Total-Count ::::::::::::::
-    $pageListUrl = "https://" . $server . ".iformbuilder.com/exzact/api/v60/profiles/$activeProfile/pages?limit=1&access_token=" . $token;
-    $pageRequestHeaders = (get_headers($pageListUrl)[4]);
-    $finalPageCount = = preg_split("/[\s,]+/", $pageRequestHeaders)[1];
+    $pageListUrl = "https://" . $server . "/exzact/api/v60/profiles/$activeProfile/pages?limit=1&access_token=" . $token;
+    $pageRequestHeaders = (get_headers($pageListUrl,1));
+    $finalPageCount = $pageRequestHeaders["Total-Count"];
     echo("Number of Total Forms:" . $finalPageCount . "\r\n");
 
 
@@ -69,7 +66,7 @@ for ($i=0; $i<$finalProfileCount; $i++){
 
     for ($j=0; $j<$finalPageCount; $j++){
       $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, "https://" . $server . ".iformbuilder.com/exzact/api/v60/profiles/$activeProfile/pages?limit=1&offset=$j");
+      curl_setopt($ch, CURLOPT_URL, "https://" . $server . "/exzact/api/v60/profiles/$activeProfile/pages?limit=1&offset=$j");
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
       curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
@@ -89,14 +86,14 @@ for ($i=0; $i<$finalProfileCount; $i++){
         //:::::::::::: For each page we get back, create the new element which is combining profile ID and page ID in dynamic value that is hidden on the device:::::::::::::::
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "https://" . $server . ".iformbuilder.com/exzact/api/v60/profiles/$activeProfile/pages/$activePage/elements");
+        curl_setopt($ch, CURLOPT_URL, "https://" . $server . "/exzact/api/v60/profiles/$activeProfile/pages/$activePage/elements");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_HEADER, FALSE);
         curl_setopt($ch, CURLOPT_POSTFIELDS, '{
-              "name": "dfa_report_key",
-              "label": "Dataflow Report Key",
-              "data_type": 1,
-              "condition_value": "false",
+              "name": "' . $elementName . '",
+              "label": "' . $elementLabel . '",
+              "data_type": ' . $dataType . ',
+              "condition_value": "' . $conditionValue . '",
               "dynamic_value": "\"' . $activeProfile . '|' . $activePage . '\""
               }');
 
